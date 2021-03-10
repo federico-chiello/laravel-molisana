387c4e7eaeb3b405c8a93e5c7c5638cf48b1463f
Fede <chiellofederico@gmail.com>
2021-03-10 16:50:52 +0100

    creo i collegamenti con il file css e js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>@yield('title', 'Pasta La Molisana')</title>
 </head>
 <body>
@@ -12,5 +13,7 @@
     <main>@yield('content')</main>
 
     @include('partials.footer')
+
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>

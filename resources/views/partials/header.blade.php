71ff220c9e6cb7d5f6ab69eafd68d556b8ca71c2
Fede <chiellofederico@gmail.com>
2021-03-11 21:53:23 +0100

    aggiunta dell'active nel css dell'header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,9 +4,9 @@
     </div>
     <div class="navbar">
         <ul>
-            <li><a href="{{ route('homepage') }}" class="{{ (Request::route()->getName() == 'homepage') ? 'active' : ''}}">Home</a></li>
-            <li><a href="{{ route('pagina-prodotti') }}" class="{{ (Request::route()->getName() == 'pagina-prodotti') ? 'active' : ''}}">Prodotti</a></li>
-            <li><a href="{{ route('pagina-news') }}" class="{{ (Request::route()->getName() == 'pagina-news') ? 'active' : ''}}">News</a></li>
+            <li><a href="{{ route('homepage') }}" class="{{ (Request::route()->getName() == 'homepage') ? 'active' : ' '}}">Home</a></li>
+            <li><a href="{{ route('pagina-prodotti') }}" class="{{ (Request::route()->getName() == 'pagina-prodotti') ? 'active' : ' '}}">Prodotti</a></li>
+            <li><a href="{{ route('pagina-news') }}" class="{{ (Request::route()->getName() == 'pagina-news') ? 'active' : ' '}}">News</a></li>
         </ul>
     </div>
 </header>
